59ace10084c7f472072dffd7957374023a465f6f
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-12 13:22:50 -0300

    usando signals para adicionar tarefas na tela

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('todo');
+  task = '';
+
+  //tasks = signal(['Fazer compras', 'Aprender Angular', 'Jogar Bola']);
+  tasks = signal(this.getTarefas());
+
+  adicionarTarefa(event: MouseEvent) {
+    event.preventDefault();
+    //this.tasks.set([...this.tasks(), this.task]);
+    this.tasks.update((t) => (t = [...t, this.task]));
+  }
+
+  getTarefas(): string[] {
+    //chamada API
+    return ['Jogar Bola', 'Lavar a Louça', 'Aprender Angular'];
+  }
 }
